6b3916869c9eed1224a7256ae3f475db2036fd92
Paula Wilson <paulafwilson@gmail.com>
2021-09-09 14:39:38 +0100

    chore: install @nuxtjs/axios

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,7 @@
 <template>
     <main>
         <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/amplienceties">Ties</NuxtLink>
+        <NuxtLink to="/nuxtmountains">Mountains</NuxtLink>
     </main>
 </template>
---
+++ pages/nuxtmountains.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Nuxt Mountains</h1>
+    <h2>Uses the fetch hook to load mountains from Nuxt's example api</h2>
+    <p>For static hosting, the fetch hook is only called during page generation, and the result is then cached for use on the client.</p>
+    <ul>
+      <li v-for="mountain of mountains" :key="mountain.slug">{{ mountain.title }}</li>
+    </ul>
+    <button @click="$fetch">Refresh</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        mountains: []
+      }
+    },
+    async fetch() {
+      this.mountains = await fetch(
+        'https://api.nuxtjs.dev/mountains'
+      ).then(res => res.json())
+    }
+  }
+</script>
